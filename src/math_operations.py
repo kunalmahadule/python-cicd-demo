c07bb3550bc05c6cf6cca541caf70e96efd76e1b
kunal <kunal@gmail.com>
2025-11-15 13:58:21 +0530

    adding yml file and correcting code

--- src/math_operations.py
+++ src/math_operations.py
@@ -1,5 +1,5 @@
-def add(x, y):
-    return x + y
+def add(a,b):
+    return a+b
 
-def subtract(x, y):
-    return x - y
+def sub(a,b):
+    return a-b 
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,11 @@
-from math_operations import add, subtract
+from src.math_operations import add, sub
 
 def test_add():
-    assert add(2, 3) == 5
-
-def test_subtract():
-    assert subtract(5, 2) == 3
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(4,3)==1
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
